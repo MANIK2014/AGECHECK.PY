99b77be48cf9e99a531682ef79998a72125eea5b
MANIK2014 <tiwarisunu@gmail.com>
2026-06-12 21:21:55 +0100

    Add files via upload

---
+++ age.py
@@ -0,0 +1,31 @@
+print("Hello")
+print("Today,I'm going to find out if your age is 10 to 20 or not")
+c=int(input("Enter your age here👉:"))
+#Note:If the age is 10 to 20 the code will print "A",If it's not print"B" 
+if c==10:
+    print("A")
+elif c ==11:
+    print("A")
+elif c ==12:
+    print("A")
+elif c ==13:
+    print("A")
+elif c ==14:
+    print("A")
+elif c ==15:
+    print("A")
+elif c ==16:
+    print("A")
+elif c==17:
+    print("A")
+elif c ==18:
+    print("A")
+elif c ==19:
+    print("A")
+elif c ==20:
+    print("A")
+else:
+    print("B")    
+     
+      
+      
